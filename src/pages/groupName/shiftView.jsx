7c0ba4b11c1c73663a55c8a3d6d15fa0b7e84e4a
YUURI Inoue <yuuri041125@gmail.com>
2025-05-27 10:25:28 +0900

    計算中の待ち部分の非同期処理を追加

---
+++ src/pages/groupName/shiftView.jsx
@@ -0,0 +1,13 @@
+import PageTitle from '@/components/PageTitle'
+import React from 'react'
+
+const ShiftView = () => {
+  return (
+      <div>
+          <PageTitle>シフト</PageTitle>
+            
+    </div>
+  )
+}
+
+export default ShiftView
