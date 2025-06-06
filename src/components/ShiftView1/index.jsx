c03e1e79ae01dbbd3a9cd10d3a8a624edc04cc0a
YUURI Inoue <yuuri041125@gmail.com>
2025-06-06 14:58:59 +0900

    importを修正

--- src/components/ShiftView1/index.jsx
+++ src/components/ShiftView1/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './shiftView1.module.css';
+import styles from './ShiftView1.module.css';
 
 const ShiftView1 = ({ shiftCompletedWithName }) => {
   if (!shiftCompletedWithName || shiftCompletedWithName.length === 0) {
