db71c80598c6855a70cea00bc4fb746ab24a0995
Yuuri Inoue <yuuri041125@gmail.com>
2025-08-23 12:46:46 +0900

    大まかな流れを実装。

--- src/components/UsageGuide/index.jsx
+++ src/components/UsageGuide/index.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 const UsageGuide = () => {
   return (
+    <div>
+      <h2>使い方</h2>
+      <pre>
+        目次 - 使い方の流れ - ページごとの説明 ・グループ作成 ・グループページ
+        ・Top ・メンバー編集 ・シフト作成 ・履歴
+      </pre>
+      {/* 使い方の流れ */}
       <div>
-          <h2>使い方</h2>
+        <h3>使い方の流れ</h3>
+      </div>
+
+      {/* ページごとの説明 */}
+      <div>
+        <h3>ページごとの説明</h3>
+      </div>
     </div>
-  )
+  );
 }
 
 export default UsageGuide
