e0ef43835a274d97048504e240550398e44213c1
Yuuri Inoue <yuuri041125@gmail.com>
2025-09-06 11:16:35 +0900

    履歴の表示を実装

--- src/components/ShiftHistory/index.jsx
+++ src/components/ShiftHistory/index.jsx
@@ -1,27 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { supabase } from 'src/lib/supabase_client';
 
-const ShiftHistory = (group_id) => {
+const ShiftHistory = ({ group_id }) => {
+  const [history, setHistory] = useState([]);
+
   useEffect(() => {
     const fetchWeeks = async () => {
       const { data, error } = await supabase
         .from('weeks')
         .select('*')
-        .eq('group_id', group_id);
+        .eq('group_id', group_id)
+        .order('week_start_date', { ascending: false }); // 週の新しい順に並べる
 
       if (error) {
         console.error('取得エラー:', error);
       } else {
         console.log('weeks データ:', data);
+        setHistory(data)
       }
-    }
-    fetchWeeks();
+    };
+    if (group_id) fetchWeeks();
   }, [group_id]);
 
   return (
     <div>
       <h2>履歴</h2>
-      <p>2025/08/15〜2025/08/22</p>
-      <p>2025/08/15〜2025/08/22</p>
+      {history.length === 0 ?
+        <p>データがありません</p>
+      :
+        history.map((item) => (
+        <div>
+          <p>{ item.week_start_date}</p>
+        </div>
+      ))}
     </div>
   );
 };
