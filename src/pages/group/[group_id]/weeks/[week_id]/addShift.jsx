2b1e28cf755a4130add978e62225758b7247d50a
Yuuri Inoue <yuuri041125@gmail.com>
2025-09-26 00:28:51 +0900

    feat: 必要人数入力のUIの変更

--- src/pages/group/[group_id]/weeks/[week_id]/addShift.jsx
+++ src/pages/group/[group_id]/weeks/[week_id]/addShift.jsx
@@ -205,14 +205,33 @@ const AddShift = () => {
           ))}
         </tbody>
       </table>
-      <div className={styles.enterTimesContainer}>
-        <p className={styles.enterTimesTitle}>入りたい回数</p>
-        <input
-          type="number"
-          value={timesToEnter}
-          className={styles.enterTimesInput}
-          onChange={(e) => setTimesToEnter(parseInt(e.target.value))}
-        />
+      <div className={styles.innerSetting}>
+        <p className={styles.settingTitle}>入りたい回数</p>
+        <label className={styles['selectbox-1']}>
+          <select onChange={(e) => setTimesToEnter(parseInt(e.target.value, 10))}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+          </select>
+        </label>
       </div>
 
       <ButtonBlue func={(e) => handleConfirm(e)}>確定</ButtonBlue>
